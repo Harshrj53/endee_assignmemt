9e90a3ed22bb74d7cf4ba7dc40f6a12ebe18d907
Harshrj53 <hrsj31@gmail.com>
2026-03-15 22:45:37 +0530

    Add root and health endpoints

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
+from fastapi import FastAPI
 from app.core.bootstrap.app import create_app
 
+app: FastAPI = create_app()
 
-app = create_app()
 
-app.get("/")(lambda: "API is up and running!")
+@app.get("/")
+def root():
+    return {"message": "API is up and running"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
